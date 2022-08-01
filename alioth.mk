3d19e80b52ab0ab8240a05e699e3f37edb2474e7
VEN0M98 <bt98.bt7@gmail.com>
2022-08-01 13:09:36 +0530

    alioth:welcome to zephyrus

--- alioth.mk
+++ alioth.mk
@@ -8,17 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_MODEL := M2102K1C
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+
+CUSTOM_BUILD_TYPE := Official
+
+# Project Zephyrua stuff.
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+TARGET_INCLUDE_WIFI_EXT := true
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -26,3 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V13.0.3.0.SKHMIXM release-keys"
 
 BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/RKQ1.200826.002/V13.0.3.0.SKHMIXM:user/release-keys
+
