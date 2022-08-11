drkphnx/device_xiaomi_alioth

sm8250-common: Build MatlogX conditionally *clone this := git clone https://github.com/AOSP-Krypton/packages_apps_MatlogX.git -b A12 packages/apps/MatlogX
Signed-off-by: drkphnx <dark.phnx12@gmail.com>

device.mk
@@ -58,5 +58,11 @@ $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# MatlogX
+ifeq ($(TAEGET_WITH_MATLOGX),true)
+PRODUCT_PACKAGES += \
+     MatlogX
+endif
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)

ageuqxb/derp-device-xiaomi-alioth

device.mk
@@ -56,5 +56,11 @@ $(call inherit-product-if-exists, vendor/GcamBSG/GcamBSG-vendor.mk)
 PRODUCT_PACKAGES += \
     SettingsOverlayRefreshRate
 
+# MatlogX
+ifeq ($(TARGET_WITH_MATLOGX),true)
+PRODUCT_PACKAGES += \
+     MatlogX
+endif
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)