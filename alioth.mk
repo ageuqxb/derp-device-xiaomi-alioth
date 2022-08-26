43ec53d4344c6be399bc3cd4e47082b67db5fdb0
VEN0M98 <bt98.bt7@gmail.com>
2022-08-26 21:36:40 +0000

    Revert "Alioth: Inherit Updatable Google Apex"
    This reverts commit 8c55399b23f1f8b9125f7c2166056a140c607dc1.

--- alioth.mk
+++ alioth.mk
@@ -8,10 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Updatable APEX
-TARGET_SHIP_PREBUILT_APEX := true
-$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
-
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
